91af46484142f5933659430568b1d62e767da23b
rongweihe <rongweihe1995@126.com>
2018-03-01 16:58:39 +0800

    Create GetPi.cpp

---
+++ GetPi.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+double eps = 1e-15;
+double pi;
+double arctan(double x)
+{
+    //arctan(x) = x-
+    double sqr = x*x;
+    double e = x;
+    double r = 0;
+    int    i = 1;
+    while(e/i > eps)
+    {
+        double f = e/i;
+        r = ( i % 4 == 1 ) ? r + f : r - f;
+        e = e*sqr;
+        i += 2;
+    }
+    return r;
+}
+pi = 16.0*arctan(1/5.0) - 4.0*arctan(1/239.0);
